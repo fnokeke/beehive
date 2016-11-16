ee7064bf33b9367f3aa2230be2857436a7f174b8
Fabian Okeke <fnokeke@gmail.com>
2016-11-15 19:54:45 -0500

    updated port no

--- runserver.py
+++ runserver.py
@@ -1,4 +1,4 @@
 from rep import app
 
 if __name__ == '__main__':
-    app.run(threaded=True, debug=True, host='0.0.0.0', port=7000)
+    app.run(threaded=True, debug=True, host='0.0.0.0', port=5000)
