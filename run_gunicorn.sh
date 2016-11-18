a655dc840dab8ac4705b0b69a4e29c1ce7afe3ae
Fabian Okeke <fnokeke@gmail.com>
2016-11-17 23:58:03 -0500

    added production deployment: gunicorn, wsgi, supervisor

---
+++ run_gunicorn.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+WORKERS=4
+WORKER_CLASS="sync"
+PORT=5000
+
+~/.virtualenvs/rep-env/bin/gunicorn \
+--bind 0.0.0.0:${PORT} \
+-w ${WORKERS} -k ${WORKER_CLASS} \
+--reload wsgi:app --access-logfile -
---
+++ wsgi.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+
+from rep import app
+
+# start the flask loop
+if __name__ == "__main__":
+    app.run()
