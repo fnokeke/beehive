3dd684f68365509b5d9b9cf6b1311e30db7044b9
Fabian Okeke <fnokeke@gmail.com>
2017-02-18 17:00:11 -0500

    fixed bugs in fallback script

--- rep/static/js/fallback.js
+++ rep/static/js/fallback.js
@@ -1,59 +1,60 @@
 (function(window, document) {
 
-  var filepath;
+    var filepath;
 
-  if (!window.jQuery) {
-    filepath = '/static/lib/js/jquery.min.js';
-    insert_script(filepath);
-  }
+    if (!window.jQuery) {
+        filepath = '/static/lib/js/jquery.min.js';
+        insert_script(filepath);
+    }
 
-  if (!window.vis) {
-    filepath = '/static/lib/js/vis.min.js';
-    insert_script(filepath);
-  }
+    if (!window.vis) {
+        filepath = '/static/lib/js/vis.min.js';
+        insert_script(filepath);
+    }
 
-  if (!window.bootstrap) {
-    filepath = '/static/lib/js/bootstrap.js';
-    insert_script(filepath);
+    if (!window.bootstrap) {
+        filepath = '/static/lib/js/bootstrap.js';
+        insert_script(filepath);
 
-    filepath = '/static/lib/css/bootstrap.min.css';
-    insert_css(filepath);
+        filepath = '/static/lib/css/bootstrap.min.css';
+        insert_css(filepath);
 
-    filepath = '/static/lib/css/vis.min.css';
-    insert_css(filepath);
+        filepath = '/static/lib/css/vis.min.css';
+        insert_css(filepath);
 
-    filepath = '/static/lib/css/bootstrap-theme.min.css';
-    insert_css(filepath);
+        filepath = '/static/lib/css/bootstrap-theme.min.css';
+        insert_css(filepath);
 
-    filepath = '/static/lib/css/bootstrap-datepicker.css';
-    insert_css(filepath);
-  }
+        filepath = '/static/lib/css/bootstrap-datepicker.css';
+        insert_css(filepath);
+    }
 
-  if (!window.datepicker) {
-    filepath = '/static/lib/js/bootstrap-datepicker.min.js';
-    insert_script(filepath);
-  }
+    if (!window.datepicker) {
+        filepath = '/static/lib/js/bootstrap-datepicker.min.js';
+        insert_script(filepath);
+    }
 
-  if (!window.confirmation) {
-    filepath = '/static/lib/js/bootstrap-confirmation.min.js';
-    insert_script(filepath);
-  }
+    if (!window.confirmation) {
+        filepath = '/static/lib/js/bootstrap-confirmation.min.js';
+        insert_script(filepath);
+    }
 
-  function insert_script2(filepath) {
-    var head = document.getElementsByTagName('head')[0];
-    var script = document.createElement('script');
-    script.src = filepath;
-    head.appendChild(script);
-  }
+    function insert_script2(filepath) {
+        var head = document.getElementsByTagName('head')[0];
+        var script = document.createElement('script');
+        script.src = filepath;
+        head.appendChild(script);
+    }
 
-  function insert_script(filepath) {
-    document.write('<script src="{0}">\x3C/script>'.format(filepath));
-  }
+    function insert_script(filepath) {
+        var js_path = '<script src="' + filepath + '"><\/script>';
+        document.write(js_path);
 
-  function insert_css(filepath) {
-    document.write('<link rel="stylesheet" href="{0}"/>'.format(filepath));
-    // document.write('<script type="text/css" src="{0}">\x3C/script>'.format(filepath));
-  }
+    }
 
+    function insert_css(filepath) {
+        var css_path = '<link rel stylesheet="' + filepath + '"/>';
+        document.write(css_path);
+    }
 
 })(window, document);
