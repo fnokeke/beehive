1917c4745e4dfaf2e7516ff1b225176b0fb12e51
Fabian Okeke <fnokeke@gmail.com>
2017-07-25 20:27:08 -0400

    gunicorn should log to files

--- run_gunicorn.sh
+++ run_gunicorn.sh
@@ -4,7 +4,7 @@ WORKERS=4
 WORKER_CLASS="sync"
 PORT=5000
 
-~/.virtualenvs/rep-aws-env/bin/gunicorn \
+~/.virtualenvs/rep-env/bin/gunicorn \
 --bind 0.0.0.0:${PORT} \
 -w ${WORKERS} -k ${WORKER_CLASS} \
---reload wsgi:app --access-logfile -
+--reload wsgi:app --access-logfile logs/access.log --error-logfile logs/error.log
