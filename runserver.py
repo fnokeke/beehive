21d3d5f1bf1c0f59cf3faa53caab032fef5f4946
Fabian Okeke <fnokeke@gmail.com>
2017-08-30 18:23:56 -0400

    change from 0.0.0.0 to localhost so google_login works

--- runserver.py
+++ runserver.py
@@ -1,4 +1,4 @@
 from rep import app
 
 if __name__ == '__main__':
-    app.run(threaded=True, debug=True, host='0.0.0.0', port=5000)
+    app.run(threaded=True, debug=True, host='127.0.0.1', port=5000)
