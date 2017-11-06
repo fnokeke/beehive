9962a729467a30d2f01c1ca32e25b38f6d9e18dd
Fabian Okeke <fnokeke@gmail.com>
2017-11-06 20:34:02 +0000

    added fake_omh_keys.py that should be renamed to omh_keys.py

---
+++ rep/fake_omh_keys.py
@@ -0,0 +1,6 @@
+#########################################################
+# fake OMH config should be renamed to omh_keys.py
+#########################################################
+OMH_CLIENT_ID = ''
+OMH_CLIENT_SECRET = ''
+OMH_DSU = 'https://production.ohmage-omh.cornelltech.io/dsu'
