edae284bc7b6b332679d35bfae08b51375f07838
nishkumar <nk595@cornell.edu>
2018-02-17 13:23:22 -0500

    Implement task Scheduler to print time

--- manage.py
+++ manage.py
@@ -1,5 +1,11 @@
 #!/usr/bin/env python
 
+
+import time
+import atexit
+from apscheduler.schedulers.background import BackgroundScheduler
+from apscheduler.triggers.interval import IntervalTrigger
+
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 
@@ -10,5 +16,24 @@ migrate = Migrate(app, db)
 manager = Manager(app)
 manager.add_command('db', MigrateCommand)
 
+def print_date_time():
+    print time.strftime("%A, %d. %B %Y %I:%M:%S %p")
+
+# Initialize taskqueue before calling flask app
 if __name__ == '__main__':
+    print "Schedule Tasks"
+    print time.strftime("%A, %d. %B %Y %I:%M:%S %p")
+    scheduler = BackgroundScheduler()
+    scheduler.start()
+    scheduler.add_job(
+        func=print_date_time,
+        trigger=IntervalTrigger(seconds=120),
+        id='printing_job',
+        name='Print date and time every five seconds',
+        replace_existing=True)
+    # Shut down the scheduler when exiting the app
+    atexit.register(lambda: scheduler.shutdown())
     manager.run()
+
+
+
--- wsgi.py
+++ wsgi.py
@@ -1,8 +1,28 @@
 #!~/.virtualenvs/rep-aws-env/bin/python
 
+import time
+import atexit
+from apscheduler.schedulers.background import BackgroundScheduler
+from apscheduler.triggers.interval import IntervalTrigger
+
 from rep import app
+
+def print_date_time():
+    print time.strftime("%A, %d. %B %Y %I:%M:%S %p")
 
 # start the flask loop
 if __name__ == "__main__":
+    print "Schedule Tasks"
+    print time.strftime("%A, %d. %B %Y %I:%M:%S %p")
+    scheduler = BackgroundScheduler()
+    scheduler.start()
+    scheduler.add_job(
+        func=print_date_time,
+        trigger=IntervalTrigger(seconds=120),
+        id='printing_job',
+        name='Print date and time every five seconds',
+        replace_existing=True)
+    # Shut down the scheduler when exiting the app
+    atexit.register(lambda: scheduler.shutdown())
     app.run(debug=True)
 
