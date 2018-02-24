e161065174fc80635298c386baca54d6b19522fa
nishkumar <nk595@cornell.edu>
2018-02-24 13:05:22 -0500

    Debug scheduler on Beehive server

--- runserver.py
+++ runserver.py
@@ -1,4 +1,7 @@
 from rep import app
+from rep.tasks_apscheduler import schedule_rescuetime_task
 
 if __name__ == '__main__':
+    print 'runserver.py'
+    schedule_rescuetime_task()
     app.run(threaded=True, debug=True, host='localhost', port=5000)
--- wsgi.py
+++ wsgi.py
@@ -5,6 +5,7 @@ from rep.tasks_apscheduler import schedule_rescuetime_task
 
 # Initialize task-queue before and run flask app
 if __name__ == "__main__":
+    print "WSGI.py"
     schedule_rescuetime_task()
     app.run(debug=True)
 
